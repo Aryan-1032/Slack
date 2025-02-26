7dd60d476a30f4a0aca32de38dd154bf244eefe1
aryan <aryankp2001@gmail.com>
2025-02-26 19:58:10 +0530

    password authentication

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -11,16 +11,50 @@ import { Separator } from "@/components/ui/separator";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { SignInFlow } from "../types";
-import { useState } from "react";
+import {  useState } from "react";
+import { TriangleAlert } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 
 interface SignUpCardProps {
   setState: (state: SignInFlow) => void;
 }
 
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
+
+    const {signIn} = useAuthActions();
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [pending , setPending] = useState(false);
+    const [error,setError] = useState("");
     const [confirmPassword,setConfirmPassword] = useState("");
+
+    const handleProviderSignUp = (value :"github"|"google") => {
+        setPending(true);
+        signIn(value)
+        .finally(() => {
+            setPending(false);
+        });
+    }
+
+    const onPasswordSignUp = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(password !== confirmPassword){
+            setError("Password do not match")
+        }
+
+        setPending(false);
+
+        signIn("password",{email,password,flow:'signUp'})
+        .catch(() => {
+            setError("Something went wrong")
+        })
+        .finally(()=>{
+            setPending(false);
+        })
+    }
+
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
@@ -29,10 +63,14 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
           Use your email or another serice to continue
         </CardDescription>
       </CardHeader>
+      {!!error && (<div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+       <TriangleAlert className="size-4"/>
+       <p>{error}</p>
+       </div>)}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form onSubmit={onPasswordSignUp} className="space-y-2.5">
           <Input
-            disabled={false}
+            disabled={pending}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
@@ -40,7 +78,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
@@ -48,7 +86,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             placeholder="Confirm Password"
@@ -62,9 +100,9 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
         <Separator />
         <div className="flex flex-col gap-y-2.5">
           <Button
-            disabled={false}
+            disabled={pending}
             value=""
-            onClick={() => {}}
+            onClick={() => handleProviderSignUp("google")}
             variant="outline"
             size="lg"
             className="w-full relative"
@@ -75,9 +113,9 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
         </div>
         <div className="flex flex-col gap-y-2.5">
           <Button
-            disabled={false}
+            disabled={pending}
             value=""
-            onClick={() => {}}
+            onClick={() => handleProviderSignUp("github")}
             variant="outline"
             size="lg"
             className="w-full relative"
